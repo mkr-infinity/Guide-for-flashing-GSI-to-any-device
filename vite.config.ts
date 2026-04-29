e6a264b9fa4d0c72fc1ceec1f3b02c6d1643e169
Kaif <125804924+mkr-infinity@users.noreply.github.com>
2026-04-29 23:44:18 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "./", // ⭐ ADD THIS LINE
+  base: "/", // ⭐ ADD THIS LINE
   
   plugins: [
     react(),
